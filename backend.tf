16d61f6787e9c34c7d2784ef79e90224f23d7d63
pmarinov555 <pmarinov555@gmail.com>
2024-12-18 20:04:39 +0000

    update

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = *CHANGE THIS TO YOUR BUCKET
+    bucket         = "pavel-kubernetes-project-v1"
     region         = "eu-west-2"
     key            = "Github-Runner-TF/terraform.tfstate"
     encrypt        = true
